e2729276fa013711a92bed8b940a7cead9a5dd75
Terry Sigle <terry@sigle.com>
2021-04-26 09:37:15 -0500

    Release 1.0.1

--- Formula/pingctl.rb
+++ Formula/pingctl.rb
@@ -1,8 +1,8 @@
 class Pingctl < Formula
   desc "pingctl - A PingIdentity CLI"
   homepage "https://pingidentity.github.io/pingctl"
-  url "https://github.com/pingidentity/pingctl/archive/v1.0.0.tar.gz"
-  sha256 "20298380bc2e96d76eb40cbe4be2dcced42f5d26a019cf8a48f10411b77e829f"
+  url "https://github.com/pingidentity/pingctl/archive/v1.0.1.tar.gz"
+  sha256 "a80223aafb6a1df04fa4105cfa82d91d322f49230146f05318f10bd4db85f200"
 
   depends_on "kubernetes-cli"
   depends_on "kubectx"
